43540491b75425ddf69caf6446c5ca89bab46719
Nikhil-ai-dev <nikhil.nn245@gmail.com>
2025-03-12 21:07:56 +0530

    Set up for spring boot application

---
+++ src/test/java/com/Prospera/ProsperaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prospera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsperaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
